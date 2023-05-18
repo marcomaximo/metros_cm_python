64bade9188e9f053d6d0d02c00c7fa7d8a568ab8
Marco Maximo <marcomaximojr@gmail.com>
2023-05-18 12:48:05 -0300

    Add file
    File added

---
+++ conversao_metros_cm.py
@@ -0,0 +1,6 @@
+#Deve-se inserir um valor numérico abaixo
+metros = int(input('Informe o valor em metros: '))
+
+
+#Temos a resposta do valor já convertido para centímetros
+print ('O valor em centímetros é:', metros * 100,'cm')
